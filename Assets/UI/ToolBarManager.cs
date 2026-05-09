8d66522fbaf20f3c98a769ea1c0e79ed8d19614b
Tanguy Le Crom <tanguy.le-crom@live.fr>
2026-05-09 13:02:14 -0400

    UI Integration

---
+++ Assets/UI/ToolBarManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public enum Tool
+{
+    Grab,
+    Shear,
+    Breed,
+    Fence
+}
+public class ToolBarManager : MonoBehaviour
+{
+    public static UnityAction<Tool> OnToolChange;
+
+    private List<Button> buttons = new();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            buttons.Add(transform.GetChild(i).GetComponent<Button>());
+        }
+        ChangeTool(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeTool(int ToolIndex)
+    {
+        OnToolChange.Invoke((Tool)ToolIndex);
+        foreach (Button button in buttons)
+            button.GetComponent<Animator>().SetBool("Current",false);
+        buttons[ToolIndex].GetComponent<Animator>().SetBool("Current", true);
+    }
+
+}
---
+++ Assets/UI/ToolCursor.cs
@@ -0,0 +1,63 @@
+using DG.Tweening.Core.Easing;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+
+public class ToolCursor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject GrabTool, ShearTool, BreedTool, FenceTool;
+    [SerializeField]
+    private Tool tool;
+
+    public Tool currentTool;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentTool = Tool.Grab;
+        ChangeTool(currentTool);
+        Cursor.visible = false;
+    }
+
+    void OnEnable()
+    {
+        ToolBarManager.OnToolChange += ChangeTool;
+
+    }
+
+    void OnDisable()
+    {
+        ToolBarManager.OnToolChange -= ChangeTool;
+    }
+
+    public void ChangeTool(Tool tool)
+    {
+        GrabTool.SetActive(false);
+        ShearTool.SetActive(false);
+        BreedTool.SetActive(false);
+        FenceTool.SetActive(false);
+
+        switch (tool)
+        {
+            case Tool.Grab:
+                GrabTool.SetActive(true); break;
+            case Tool.Shear:
+                ShearTool.SetActive(true); break;
+            case Tool.Breed:
+                BreedTool.SetActive(true); break;
+            case Tool.Fence:
+                FenceTool.SetActive(true); break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        transform.position = mousePos;
+    }
+}
